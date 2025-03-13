d8460a05252be2e0aa46504983130727e873a8b5
yusufsenturk <yusuf.joe6161@gmail.com>
2025-03-13 16:13:09 +0300

    ForgotViewController Designed

---
+++ Furniture/ForgotViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ForgotViewController.swift
+//  Furniture
+//
+//  Created by yusuf Şentürk on 13.03.2025.
+//
+
+import UIKit
+
+class ForgotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
