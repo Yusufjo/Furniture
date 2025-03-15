9a6c54a4edc2a0a43774cf5557b9113efa09128d
yusufsenturk <yusuf.joe6161@gmail.com>
2025-03-15 19:24:54 +0300

    forgot View controller added

--- Furniture/ForgotViewController.swift
+++ Furniture/ForgotViewController.swift
@@ -11,13 +11,22 @@ class ForgotViewController: UIViewController {
     @IBOutlet weak var forgotView: UIView!
     @IBOutlet weak var emailTextField: UITextField!
     
+    @IBOutlet weak var forgotPasswordLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        localize()
         forgotView.addShadow()
     }
 
     @IBAction func resetPasswordButtonTapped(_ sender: Any) {
     }
     
+    func localize(){
+        forgotPasswordLabel.localize(localizableID: "forgotLabel")
+        resetButton.localize(localizableID: "reset")
+        emailLabel.localize(localizableID: "email")
+    }
 }
