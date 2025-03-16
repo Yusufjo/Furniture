349d7890e08a73268f98c5fb46ebb94e527281a5
Ömer Okumuş <omerokumus3@gmail.com>
2025-03-16 03:18:54 +0300

    C class for constants

---
+++ Furniture/base/C.swift
@@ -0,0 +1,14 @@
+//
+//  C.swift
+//  Furniture
+//
+//  Created by Ömer Faruk Okumuş on 16.03.2025.
+//
+
+import Foundation
+
+
+class C {
+    static let BASE_URL = "https://furniture-shopping-spring.onrender.com/api/"
+}
+
